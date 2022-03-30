49d30d1346ab820f6c04a18e335ff95d28e3edd3
Zulhelmi <zulhelmi@uitm.edu.my>
2022-03-30 11:24:26 +0800

    change service worker

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,65 @@
-// Choose a cache name
-const cacheName = 'cache-v1';
-// List the files to precache
-const precacheResources = ['/', '/index.html', 'assets/css/style.css', 'assets/js/main.js'];
+var APP_NAME = 'myResume';
+var APP_VER = '0.1';
+var CACHE_NAME = APP_NAME + '-' + APP_VER;
 
-// When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+const REQUIRED_FILES = ['/', '/index.html', 'assets/css/style.css', 'assets/js/main.js'];
+
+// Service Worker Diagnostic. Set true to get console logs.
+var APP_DIAG = false;
+
+//Service Worker Function Below.
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(function(cache) {
+			//Adding files to cache
+			return cache.addAll(REQUIRED_FILES);
+		}).catch(function(error) {
+			//Output error if file locations are incorrect
+			if(APP_DIAG){console.log('Service Worker Cache: Error Check REQUIRED_FILES array in _service-worker.js - files are missing or path to files is incorrectly written -  ' + error);}
+		})
+		.then(function() {
+			//Install SW if everything is ok
+			return self.skipWaiting();
+		})
+		.then(function(){
+			if(APP_DIAG){console.log('Service Worker: Cache is OK');}
+		})
+	);
+	if(APP_DIAG){console.log('Service Worker: Installed');}
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		//Fetch Data from cache if offline
+		caches.match(event.request)
+			.then(function(response) {
+				if (response) {return response;}
+				return fetch(event.request);
+			}
+		)
+	);
+	if(APP_DIAG){console.log('Service Worker: Fetching '+APP_NAME+'-'+APP_VER+' files from Cache');}
 });
 
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    }),
-  );
+self.addEventListener('activate', function(event) {
+	event.waitUntil(self.clients.claim());
+	event.waitUntil(
+		//Check cache number, clear all assets and re-add if cache number changed
+		caches.keys().then(cacheNames => {
+			return Promise.all(
+				cacheNames
+					.filter(cacheName => (cacheName.startsWith(APP_NAME + "-")))
+					.filter(cacheName => (cacheName !== CACHE_NAME))
+					.map(cacheName => caches.delete(cacheName))
+			);
+		})
+	);
+	if(APP_DIAG){console.log('Service Worker: Activated')}
 });
+
+self.addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+      self.skipWaiting();
+    }
+  });
